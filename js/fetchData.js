fb7a67b0ecb8c0cf71d1b92dc4cfb65da4121902
Leinad <danielli12750@gmail.com>
2022-09-27 22:48:36 +1000

    debuging

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,5 +1,8 @@
 export async function fetchData() {
+  console.log("parsing");
   let data = JSON.parse(localStorage.getItem("data"));
+  console.log("parse completed");
+
   if (!data) {
     data = await fetch(
       "http://127.0.0.1:5500/interactive-comments-section-main/data.json"
